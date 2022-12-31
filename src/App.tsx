8cf45ea0dcf15e0cace49093db4d6ea2195ddef6
jaden <jadencho1014@gmail.com>
2022-12-31 13:39:40 -0500

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/w-findit.png";
+import "./App.css";
+import { Icon } from "@iconify/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world this is jaeden.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <img src={logo} alt="logo" className="logo" />
+          </div>
+
+          <div>
+            <div>
+              <Icon icon="ic:outline-email" color="#ccc" className="email" />
+              <input
+                type="text"
+                placeholder="UWaterloo Email"
+                className="name"
+              />
+            </div>
+            <div className="second-input">
+              <Icon icon="carbon:password" color="#ccc" className="email" />
+              <input type="password" placeholder="Password" className="name" />
+            </div>
+            <div className="login-button">
+              <button>Login</button>
+            </div>
+            <p className="link">
+              <a href="#">Forgot password?</a> or <a href="#">Sign up</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
